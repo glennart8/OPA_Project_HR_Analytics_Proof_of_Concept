f561ba3672c1f03aab34acf0026c81cc76a1c04e
Jonas Gustafsson <jonas.gustafsson85@gmail.com>
2025-05-05 12:02:44 +0200

    Created a mart model that shows vacancies by city

--- HT_analytics/target/compiled/HT_analytics/models/dim/dim_auxilliary_attributes.sql
+++ HT_analytics/target/compiled/HT_analytics/models/dim/dim_auxilliary_attributes.sql
@@ -2,11 +2,6 @@ WITH  __dbt__cte__src_auxilliary_attributes as (
 with stg_auxilliary_attributes as (select * from "job_ads"."staging"."job_ads")
 
 select  
-<<<<<<< HEAD
-=======
-        -- id_aux, -- hittar på ett id
-        -- id,
->>>>>>> 06b84b6770a2b987bd1fe052c4f1d270650950ba
         experience_required,
         driving_license_required as driver_license,
         access_to_own_car
