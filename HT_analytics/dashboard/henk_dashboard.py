e182ed3062df661f8fbe1ef4185320bd15a53695
glennart8 <henrikpilback@gmail.com>
2025-05-09 10:25:12 +0200

    Got the dashboard filter-function to work now, will keep changing it, but now it works

--- HT_analytics/dashboard/henk_dashboard.py
+++ HT_analytics/dashboard/henk_dashboard.py
@@ -5,9 +5,9 @@ import streamlit as st
 
 con = duckdb.connect('../job_ads.duckdb')
 
-# # BY CITY
-# df_by_city = con.execute("SELECT * FROM marts.mart_vacancies_by_city")
-# st.dataframe(df_by_city)
+# 1. Välj kommun, fält och yrkestitel
+# 2. Visa dessa tillsammans med namnet på företaget och en headline
+
 
 
 # HÄMTA UNIKA KOMMUNER
@@ -17,6 +17,8 @@ municipalities = con.execute("""
 """).fetchdf()['workplace_municipality'].dropna().sort_values().tolist()
 
 municipality_filter = st.selectbox("Välj kommun:", municipalities)
+municipality_filter_lower = municipality_filter.lower()
+st.write("Vald kommun:", municipality_filter)
 
 # HÄMTA UNIKA YRKESFÄLT
 occupation_fields = con.execute("""
@@ -25,6 +27,7 @@ occupation_fields = con.execute("""
 """).fetchdf()['occupation_field'].dropna().sort_values().tolist()
 
 occupation_field_filter = st.selectbox("Välj yrkesfält:", occupation_fields)
+st.write("Valt yrkesfält:", occupation_field_filter)
 
 # HÄMTA UNIKA YRKEN
 occupations = con.execute("""
@@ -34,15 +37,42 @@ occupations = con.execute("""
 """, (occupation_field_filter,)).fetchdf()['occupation'].unique()
 
 occupation_filter = st.selectbox("Välj yrkeskategori:", occupations)
+st.write("Vald yrkeskategori:", occupation_filter)
 
 # VISA ARBETEN EFTER ATT DE FILTRERATS
 filtered_jobs = con.execute("""
     SELECT *
     FROM marts.mart_vacancies_by_mun_field_occ
     WHERE workplace_municipality = ?
-    AND occupation_field = ?
     AND occupation = ?
-""", (municipality_filter, occupation_field_filter, occupation_filter)).fetchdf()
+    ORDER BY occupation
+""", (municipality_filter_lower, occupation_filter)).fetchdf()
+
+# För att dölja municipality, måste finnas med i marts för att filtrera
+filtered_jobs = filtered_jobs.drop(columns=["workplace_municipality"])
 
 # Visa de filtrerade jobben
 st.dataframe(filtered_jobs)
+
+
+
+### TEST
+
+# test_jobs2 = con.execute("""
+# SELECT 
+#     -- LOWER(e.workplace_municipality) AS workplace_municipality,
+#     -- o.occupation_field,
+#     o.occupation,
+#     e.employer_name,
+#     e.employer_workplace,
+#     m.vacancies
+# FROM refined.fct_job_ads m
+# JOIN refined.dim_employer e ON m.employer_id = e.employer_id
+# JOIN refined.dim_occupation o ON m.occupation_id = o.occupation_id
+# WHERE LOWER(e.workplace_municipality) = ?
+# ORDER BY o.occupation
+# """, (municipality_filter_lower,)).fetchdf()
+
+# st.dataframe(test_jobs2)
+
+
