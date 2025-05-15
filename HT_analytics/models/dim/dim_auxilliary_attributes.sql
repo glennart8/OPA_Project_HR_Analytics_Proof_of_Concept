45186a34708c953eb898fd225800ff93b046d738
glennart8 <henrikpilback@gmail.com>
2025-05-15 08:36:17 +0200

    Changed the mode to pick DISTINCT aux_atrributes

--- HT_analytics/models/dim/dim_auxilliary_attributes.sql
+++ HT_analytics/models/dim/dim_auxilliary_attributes.sql
@@ -1,6 +1,6 @@
 WITH aux as (SELECT * FROM {{ref ('src_auxilliary_attributes')}})
 
-select 
+select distinct
     {{dbt_utils.generate_surrogate_key(['experience_required', 'driver_license', 'access_to_own_car'])}} as id_aux,
     experience_required,
     driver_license,
