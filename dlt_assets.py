2e046c115c4ea1d4a41652a287271cbd30e6b8a6
glennart8 <henrikpilback@gmail.com>
2025-05-28 11:46:38 +0200

    Took away lots of file, and moved the orchestration stuff tothe root-folder, changedf assetkeys here as well, added the context thing in dlt_load()

---
+++ dlt_assets.py
@@ -0,0 +1,35 @@
+import dlt
+from pathlib import Path
+import sys
+import dagster as dg
+from dagster import define_asset_job  # <-- Lägg till denna import!
+from dagster_dlt import DagsterDltResource, dlt_assets
+
+# === DATABASE ===
+db_path = str(Path(__file__).parent / "HT_analytics/job_ads.duckdb")
+
+# === IMPORTERA DIN DLT-KÄLLA ===
+from pipeline import jobads_source
+
+dlt_resource = DagsterDltResource()
+
+# === DAGSTER-ASSET ===
+@dlt_assets(
+    dlt_source=jobads_source(),
+    dlt_pipeline = dlt.pipeline( # HÄR AHDE VI SKRIVIT PIPELINE BARA
+        pipeline_name="jobads_hits",
+        dataset_name="staging",
+        destination=dlt.destinations.duckdb(db_path),
+    ),
+)
+def dlt_load(context: dg.AssetExecutionContext, dlt: DagsterDltResource): # MÅSTE HA EMD CONTEXT OCH RESOURCE
+    yield from dlt.run(context=context) #yield all items from running the pipeline
+
+# === JOB ===
+dlt_job = dg.define_asset_job("dlt_job", selection=dg.AssetSelection.keys("dlt_jobads_source_jobsearch_resource")) # BEHÖVER HETA SOM FUNKTIONERNA
+
+# === SCHEDULE ===
+schedule_dlt = dg.ScheduleDefinition(
+    job=dlt_job,
+    cron_schedule="33 9 * * *" #UTC
+)
